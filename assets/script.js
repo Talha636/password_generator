2aedd5ac5e37225fa92cd7ff245ff2dab963eff2
Talha636 <85548877+Talha636@users.noreply.github.com>
2021-09-20 00:39:16 -0400

    JS done

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,65 @@
+// Declaring all possible characters, numbers and
+function getLower() {
+  let lower = 'abcdefghijklmnopqrstuvxyz';
+  return lower[Math.floor(Math.random() * lower.length)];
+}
+function getUpper() {
+  let upper = 'ABCDEFGHIJKLMNOPQRSTUVXYZ';
+  return upper[Math.floor(Math.random() * upper.length)];
+}
+function getNumber() {
+  let number = '1234567890';
+  return number[Math.floor(Math.random() * number.length)];
+}
+function getSymbol() {
+  let symbols = "!@#$%^&*()";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
 // Assignment Code
-let lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z'];
 let generateBtn = document.querySelector("#generate");
-
 
 function generatePassword() {
   // ask user for length
-  let passLength = prompt('Select a number between 8 and 128 to be your password length.');
-  console.log(passLength);
+  let length = prompt('Select a number between 8 and 128 to be your password length.');
   // validate length to be atleast 8 and less than 128
-  if (passLength < 8 || passLength > 128) {
+  if (length < 8 || length > 128) {
     alert('Please enter a number between 8 and 128.');
-    generatePassword();
     return;
+  } else {
+    // ask lowercase, uppercase, number, special characters
+    let lower = confirm('Would you like lowercase letters?');
+    let upper = confirm('Would you like UPPERCASE letters?');
+    let number = confirm('Would you like numbers?');
+    let symbol = confirm('Would you like special characters letters?');
+    // validate atleast one option
+    if (!lower && !upper && !number && !symbol) {
+      alert('You must choose "OK" for one option.');
+      return;
+    }
+    return makePassword(length, lower, upper, number, symbol);
   }
-  // ask lowercase, uppercase, number, special characters
-  let passLower = confirm('Would you like lowercase letters?');
-  let passUpper = confirm('Would you like UPPERCASE letters?');
-  let passNumber = confirm('Would you like numbers?');
-  let passSymbols = confirm('Would you like special characters letters?');
-  // validate atleast one option
-  if (!passLower && !passUpper && !passNumber && !passSymbols) {
-    alert('You must choose "OK" for one option.');
-    generatePassword();
-  }
-  // make function
-  let pass = '';
-  for (let i = 0; i < passLength; i++) {
-    pass += passLower ? lower : '';
-  }
-  // 1. make new array with the characters for the options that are accepted
-  // 2. makw new array to push randomly at least one character from the selected options arrays
-  // 3. push randomly according to length in a loop to a new array
-
-  // return the new password somewhere in the html element or alert
-  return (pass.join(''));
 }
 
+function makePassword(length, lower, upper, number, symbols) {
+  let pass = "";
+
+  for (let i = 0; i < length; i++) {
+    pass += lower ? getLower() : "";
+    pass += upper ? getUpper() : "";
+    pass += number ? getNumber() : "";
+    pass += symbols ? getSymbol() : "";
+  }
+
+  return pass.slice(0, length)
+}
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword()
+  let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// make function
-
-// 1. make new array with the characters for the options that are accepted
-// 2. makw new array to push randomly at least one character from the selected options arrays
-// 3. push randomly according to length in a loop to a new array
-
-// return the new password somewhere in the html element or alert
-
